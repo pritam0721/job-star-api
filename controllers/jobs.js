15e74cb4421d8983507373ed40f9b1824cfd981e
pritam0721 <pritamgayen629@gmail.com>
2024-07-18 18:35:07 +0530

    final  commit of jobster api

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,7 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
+const mongoose = require("mongoose");
+const moment = require("moment");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
@@ -109,10 +111,56 @@ const deleteJob = async (req, res) => {
   res.status(StatusCodes.OK).send();
 };
 
+const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+let monthlyApplications = await Job.aggregate([
+  { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+  {
+    $group: {
+      _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+      count: { $sum: 1 },
+    },
+  },
+  { $sort: { "_id.year": -1, "_id.month": -1 } },
+  { $limit: 6 },
+]);
+
+  monthlyApplications = monthlyApplications.map((item) => {
+    const { _id:{year, month} , count } = item;
+
+    const date = moment()
+                .month(month-1)
+                .year(year)
+                .format('MMM y')
+
+                return {date , count}
+  })
+  .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
+};
+
 module.exports = {
   createJob,
   deleteJob,
   getAllJobs,
   updateJob,
   getJob,
+  showStats,
 };
